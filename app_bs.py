d1672af0f151c07bf6bd9764ac02001b9ab27d78
unknown <antonykumar15898@gmail.com>
2024-04-18 15:16:26 +0530

    beatiful soup app

--- app_bs.py
+++ app_bs.py
@@ -2,3 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 
+website = "https://subslikescript.com/movie/Titanic-120338"
+result = requests.get(website)
+content = result.text
+
+soup = BeautifulSoup(content, 'lxml')
+print(soup.prettify())
+
