9ee485d8825b9b26c4ea02cb37e6d66a627f4a34
Antony Praveenkumar <antonykumar15898@gmail.com>
2024-04-25 13:01:24 +0530

    selenium

--- app_selenium_audiable.py
+++ app_selenium_audiable.py
@@ -1,6 +1,7 @@
 import os
 import traceback
 import pandas as pd
+import typing as T
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -34,5 +35,31 @@ WebDriverWait(driver, 5).until(
 # get all the cards
 audio_cards = driver.find_elements(By.CSS_SELECTOR, "li.productListItem")
 
+final_data = []
 for card in audio_cards:
-    print(card)
+    temp = {
+        "image_url": card.find_element(By.CSS_SELECTOR, "picture > img").get_attribute("src")
+    }
+
+    final_data.append(temp)
+
+
+def create_file(data:T.List[T.Dict], is_json:bool=None)->None:
+    """
+        This function is used to create file using the scraped data
+
+        Args:
+            data (list[dict]) : The data contains the list of dict with scraped data
+            is_json (bool) : To create a file as a `.json` format file
+    """
+
+    # Create a data frame
+    df = pd.DataFrame(data)
+
+    file_name = "./temp/app_selenium_audiable"
+    if is_json:
+        df.to_json(file_name+".json")
+    else:
+        df.to_excel(file_name+".xlsx", index=True)
+
+create_file(final_data)
